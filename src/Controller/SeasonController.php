f1d4fd8d700ae8b10907f4886b9cfa1e93bf135f
Delphinus_dev <delphine.gobbin@gmail.com>
2020-01-28 10:42:27 +0100

    brouillon fixtures

--- src/Controller/SeasonController.php
+++ src/Controller/SeasonController.php
@@ -10,13 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/season")
- */
 class SeasonController extends AbstractController
 {
     /**
-     * @Route("/", name="season_index", methods={"GET"})
+     * @Route("/season", name="season_index", methods={"GET"})
      */
     public function index(SeasonRepository $seasonRepository): Response
     {
